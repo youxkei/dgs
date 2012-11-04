e2d8da34b84918f8fa16bc20a0c7810d95edef8b
youkei <youxkei@gmail.com>
2012-11-05 03:14:00 +0900

    Update lib name

--- example/main.d
+++ example/main.d
@@ -7,23 +7,23 @@ debug import std.stdio;
 void main(){
     initDgs(640, 480, true);
     scope(exit) destroyDgs();
-	setWindowTitle("dgs test");
-	mouseAliases[BUTTON_LEFT] = KEY_z;
-	auto sp = (new Sprite).field!("image", "centerX", "centerY").set(new Image("youkei.png"), 128, 128);
-	auto sp2 = (new Sprite).field!"image".set(new Image("テキスト描写成功!!", 64));
-	openWindow();
-	while(true){
-		processEvents();
-		clearWindow();
-		sp.rotate += 2;
-		if(isKeyRepeated(KEY_DOWN)){
-			writeln("passed");
-		}else if(isKeyTriggered(KEY_z)){
-			writeln("z");
-		}
-		sp.draw();
-		sp2.draw();
-		updateWindow();
-	}
+    setWindowTitle("dgs test");
+    mouseAliases[BUTTON_LEFT] = KEY_z;
+    auto sp = (new Sprite).field!("image", "centerX", "centerY").set(new Image("youkei.png"), 128, 128);
+    auto sp2 = (new Sprite).field!"image".set(new Image("テキスト描写成功!!", 64));
+    openWindow();
+    while(true){
+        processEvents();
+        clearWindow();
+        sp.rotate += 2;
+        if(isKeyRepeated(KEY_DOWN)){
+            writeln("passed");
+        }else if(isKeyTriggered(KEY_z)){
+            writeln("z");
+        }
+        sp.draw();
+        sp2.draw();
+        updateWindow();
+    }
 }
 
--- src/dgs/all.d
+++ src/dgs/all.d
@@ -19,36 +19,36 @@ public import dgs.Window;
 
 void initDgs(int width, int height, bool vsync){
     DerelictGL.load();
-    //DerelictGL.reload();
     chdir("lib");
     version(Posix){
         DerelictSDL2.load("./libSDL2.so");
         DerelictSDL2ttf.load("./libSDL2_ttf.so");
+        DerelictIL.load("libIL.so,libIL.so.1");
     }else{
         DerelictSDL2.load();
         DerelictSDL2ttf.load();
+        DerelictIL.load();
     }
-	DerelictIL.load();
-	chdir("..");
+    chdir("..");
 
-	enforce(!SDL_Init(SDL_INIT_VIDEO));
+    enforce(!SDL_Init(SDL_INIT_VIDEO));
     enforce(!SDL_InitSubSystem(SDL_INIT_JOYSTICK));
     enforce(!SDL_InitSubSystem(SDL_INIT_AUDIO));
     enforce(!TTF_Init());
     ilInit();
 
-	enforce(!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true));
+    enforce(!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true));
     window = enforce(SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN));
     context = enforce(SDL_GL_CreateContext(window));
 
-	glCheck!glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
-	glCheck!glClearColor( 0.0, 0.0, 0.0, 1.0 );
-	glCheck!glEnable(GL_TEXTURE_2D);
-	glCheck!glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glCheck!glEnable(GL_BLEND);
-	glCheck!glDisable(GL_DEPTH_TEST);
+    glCheck!glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
+    glCheck!glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    glCheck!glEnable(GL_TEXTURE_2D);
+    glCheck!glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glCheck!glEnable(GL_BLEND);
+    glCheck!glDisable(GL_DEPTH_TEST);
 
-	glCheck!glClear(GL_COLOR_BUFFER_BIT);
+    glCheck!glClear(GL_COLOR_BUFFER_BIT);
     SDL_GL_SetSwapInterval(vsync ? 1 : 0);
     SDL_GL_SwapWindow(window);
 
@@ -67,19 +67,19 @@ void destroyDgs(){
 }
 
 void processEvents(){
-	updateKeyRepeat();
-	SDL_Event levent;
-	while(SDL_PollEvent(&levent)){
-		switch(levent.type){
-			case SDL_KEYDOWN:
-			case SDL_KEYUP:
-			case SDL_JOYBUTTONDOWN:
-			case SDL_JOYBUTTONUP:
-			case SDL_JOYAXISMOTION:
-			case SDL_JOYHATMOTION:
-			case SDL_MOUSEBUTTONDOWN:
-			case SDL_MOUSEBUTTONUP:
-			case SDL_MOUSEMOTION:
+    updateKeyRepeat();
+    SDL_Event levent;
+    while(SDL_PollEvent(&levent)){
+        switch(levent.type){
+            case SDL_KEYDOWN:
+            case SDL_KEYUP:
+            case SDL_JOYBUTTONDOWN:
+            case SDL_JOYBUTTONUP:
+            case SDL_JOYAXISMOTION:
+            case SDL_JOYHATMOTION:
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+            case SDL_MOUSEMOTION:
                 processInputEvent(levent);
                 break;
             case SDL_WINDOWEVENT:
@@ -90,8 +90,8 @@ void processEvents(){
             case SDL_QUIT:
                 throw new Exception("quit");
             default:
-		}
-	}
+        }
+    }
 }
 
 
